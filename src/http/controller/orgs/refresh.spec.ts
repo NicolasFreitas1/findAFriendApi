91904b708bbd53339c06bed7a2353c230045f561
Nicolas Freitas <nicolas.freitas@agpr5.com>
2023-07-20 11:19:52 -0300

    :sparkles: feat: refresh token

---
+++ src/http/controller/orgs/refresh.spec.ts
@@ -0,0 +1,45 @@
+import request from "supertest";
+import { app } from "@/app";
+import { beforeAll, describe, expect, it } from "vitest";
+import { afterEach } from "node:test";
+
+describe("Refresh token (e2e)", () => {
+  beforeAll(async () => {
+    await app.ready();
+  });
+
+  afterEach(async () => {
+    await app.close();
+  });
+
+  it("should be able to refresh a token", async () => {
+    await request(app.server).post("/orgs").send({
+      name: "Pet Org",
+      email: "org@test.com",
+      password: "123456",
+      phone: "4899999999",
+      cep: "88813581",
+      address: "Rua Antonio José Olímpio",
+    });
+
+    const authResponse = await request(app.server).post("/sessions").send({
+      email: "org@test.com",
+      password: "123456",
+    });
+
+    const cookies = authResponse.get("Set-Cookie");
+
+    const response = await request(app.server)
+      .patch("/token/refresh")
+      .set("Cookie", cookies)
+      .send();
+
+    expect(response.statusCode).toEqual(200);
+    expect(response.body).toEqual({
+      token: expect.any(String),
+    });
+    expect(response.get("Set-Cookie")).toEqual([
+      expect.stringContaining("refreshToken="),
+    ]);
+  });
+});
--- src/http/controller/orgs/routes.ts
+++ src/http/controller/orgs/routes.ts
@@ -1,8 +1,16 @@
 import { FastifyInstance } from "fastify";
 import { register } from "./register";
 import { authenticate } from "./authenticate";
+import { profile } from "./profile";
+import { verifyJWT } from "@/http/middlewares/verify-jwt";
+import { refresh } from "./refresh";
 
 export async function orgsRoutes(app: FastifyInstance) {
   app.post("/orgs", register);
   app.post("/sessions", authenticate);
+
+  app.patch("/token/refresh", refresh);
+
+  /** Authenticated */
+  app.get("/me", { onRequest: [verifyJWT] }, profile);
 }
